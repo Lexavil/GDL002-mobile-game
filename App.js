3fbc31dde5ec0c72cebcc6b444165d5423fcd53d
aviles.esp@gmail.com <aviles.esp@gmail.com>
2019-06-28 02:41:01 -0500

    actualizacion

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, Dimensions, Animated, Image, } from 'react-native';
-import Enemy from './app/Components/Enemy';
+import Enemy from './app/components/Enemy';
 
 export default class alexapp extends Component {
 
@@ -49,7 +49,7 @@ constructor(props) {
 
         <View style={styles.left}>
           <Text style={styles.left} onPress={ () => this.movePlayer('left') }> {'<'} </Text>
-          <Text style={styles.right} onPress={ () => this.movePlayer('right') }>> {'>'} </Text>
+          <Text style={styles.right} onPress={ () => this.movePlayer('right') }> {'>'} </Text>
         </View>
 
       </Image>
@@ -83,10 +83,70 @@ movePlayer(direction) {
 
   }
 }
-
+componentDidMount() {
+  this.animatedEnemy();
 }
 
-const style = StyleSheet.create({
+animatedEnemy() {
+  this.state.moveEnemyval.setValue(-100);
+  var windowH = Dimensions.get('window').height;
+
+  //Distancia del enemigo
+  var r = Math.floor(Math.random() * 2) + 1;
+
+  if (r == 2) {
+    r = 40;
+    this.setState({ enemySide: 'left' });
+  } else {
+    r = Dimensions.get('window').width -140;
+    this.setState({ enemySide: 'right'  });
+  }
+  this.setState({ enemyStartposX: r });
+  
+    var refreshIntervalId;
+    refreshIntervalId = setInterval( () => {
+
+      if (this.state.moveEnemyval._value > windowH - 280
+        && this.state.moveEnemyval._value < windowH -180
+        && this.state.playerSide == this.state.enemySide) {
+          
+          clearInterval(refreshIntervalId)
+          this.setState({ gameOver: true });
+          this.gameOver();
+        }
+
+    }, 50);
+
+    //incrementar la velocidad despues de 20sg
+    setInterval( () => {
+      this.setState({ enemySpeed: this.state.enemySpeed -50 })
+    }, 20000);
+
+    //Animacion del enemigo
+    Animated.timing(
+      this.state.moveEnemyval,
+      {
+        toValue: Dimensions.get('window').height,
+        duration: this.state.enemySpeed,
+      }
+    ).start(event => {
+      if (event.finished && this.state.gameOver == false) {
+        clearInterval(refreshIntervalId);
+        this.setState({ points: ++this.state.points });
+        this.animatedEnemy();
+      }
+    });
+
+  }
+
+  gameOver() {
+    alert('Te aplastaron!');
+  }
+
+  }
+
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: 'relative',
@@ -123,4 +183,5 @@ const style = StyleSheet.create({
 
 
 });
+
 AppRegistry.registerComponent('alexapp', () => alexapp);
